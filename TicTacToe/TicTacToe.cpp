8c902984b0ae941d33f3fe6dc06edec0d6a21b3e
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-04-06 20:47:17 +0800

    Add files via upload

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,54 +1,72 @@
 #include <iostream>
 #include<vector>
 #include<cctype>
+
+
 struct boardConstructor {
 private:
-    const int height = 3;
-    const int width = 3;
+	const int height = 3;
+	const int width = 3;
 public:
-    std::vector<std::vector<bool>> board
-    {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    };
+	std::vector<std::vector<char>> board
+	{
+	{'a', 'a', 'a'},
+	{'a', 'a', 'a'},
+	{'a', 'a', 'a'}
+	};
 
-    void printToConsole() {
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                std::cout << board[i][j] << "";
-            }
-            std::cout << "\n";
-        }
-    }
 
-    void updateBoard(int8_t x, int8_t y) {
-        printToConsole();
-    }
+	void printToConsole() {
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				std::cout << board[i][j] << "";
+			}
+			std::cout << "\n";
+		}
+	}
+
+
+	void updateBoard(int8_t x, int8_t y, int8_t whichPlayer) {
+		switch (whichPlayer) {
+		case 1:
+			board[y][x] = 'x';
+			break;
+		case 2:
+			board[y][x] = 'y';
+			break;
+		default:
+			std::cout << "Pick a valid option";
+		}
+		printToConsole();
+	}
 };
 
+
+int8_t whichPlayerGlobal = 1;
 boardConstructor GameBoard;
+
 
 void chooseWhichOption();
 
+
 void chooseWhichOption() {
-    std::string chosenSquare;
-    int8_t chosenX;
-    int8_t chosenY;
-    std::cout << "What square do you want to pick";
-    std::cout << "X axis";
-    std::cin >> chosenX;
-    std::cout << "Y axis";
-    std::cin >> chosenY;
-    std::cin >> chosenSquare;
-    GameBoard.updateBoard(chosenX, chosenY);
+	std::string chosenSquare;
+	int8_t chosenX;
+	int8_t chosenY;
+	std::cout << "What square do you want to pick";
+	std::cout << "X axis";
+	std::cin >> chosenX;
+	std::cout << "Y axis";
+	std::cin >> chosenY;
+	GameBoard.updateBoard(chosenX, chosenY, whichPlayerGlobal);
 }
 
 
+
 int main() {
-    GameBoard.printToConsole();
-    chooseWhichOption();
-    std::cout << "Tic tac toe";
+	GameBoard.printToConsole();
+	chooseWhichOption();
+	std::cout << "Tic tac toe";
 }
