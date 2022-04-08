9f8e515620af8a3a27c36926a16baaaf9a7d8554
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-04-08 11:37:43 +0800

    Add files via upload

--- tic tac toe/tic tac toe.cpp
+++ tic tac toe/tic tac toe.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include<vector>
 #include<cctype>
+
 struct boardConstructor {
 private:
 	const int height = 3;
 	const int width = 3;
 public:
-	std::vector<std::vector<char>> board
-	{
-	{'a', 'a', 'a'},
-	{'a', 'a', 'a'},
-	{'a', 'a', 'a'}
+	char board[3][3] = {
+		{'o','o','o'},
+		{'o','o','o'},
+		{'o','o','o'}
 	};
 	void printToConsole() {
 		for (int i = 0; i < height; i++)
@@ -22,16 +22,23 @@ public:
 			std::cout << "\n";
 		}
 	}
+
 	void updateBoard(int x, int y, int whichPlayer) {
-		switch (whichPlayer) {
-		case 1:
-			board[y][x] = 'x';
-			break;
-		case 2:
-			board[y][x] = 'y';
-			break;
-		default:
+		x--;
+		y--;
+		if (x <= width && y <= height) {
+			switch (whichPlayer) {
+			case 1:
+				board[y][x] = 'x';
+				break;
+			case 2:
+				board[y][x] = 'y';
+				break;
+			}
+		}
+		else if (x > width || y > height) {
 			std::cout << "Pick a valid option\n";
+			Player1.chooseWhichOption();
 		}
 		printToConsole();
 	}
@@ -40,7 +47,7 @@ public:
 class PlayerCreator {
 int8_t wins = 0; 
 public:
-	void checkWinner(std::vector < std::vector<char> > boardLocal) {
+	void checkWinner(char boardLocal[3][3]) {
 		int8_t howManyX = 0;
 		int8_t howManyY = 0;
 		for (int i = 0; i < 3; i++) {
@@ -54,12 +61,25 @@ public:
 			}
 		}
 		if (howManyX >= 3) {
-
+			
 		}
 
 		if (howManyY >= 3) {
 
 		}
+	}
+
+	void chooseWhichOption() {
+		std::string chosenSquare;
+		int8_t chosenX;
+		int8_t chosenY;
+		std::cout << "What square do you want to pick\n";
+		std::cout << "X axis\n";
+		std::cin >> chosenX;
+		std::cout << "Y axis\n";
+		std::cin >> chosenY;
+		GameBoard.updateBoard(chosenX, chosenY, whichPlayerGlobal);
+
 	}
 };
 
@@ -68,23 +88,8 @@ boardConstructor GameBoard;
 PlayerCreator Player1;
 PlayerCreator Player2;
 
-void chooseWhichOption();
-
-void chooseWhichOption() {
-	std::string chosenSquare;
-	int8_t chosenX;
-	int8_t chosenY;
-	std::cout << "What square do you want to pick\n";
-	std::cout << "X axis\n";
-	std::cin >> chosenX;
-	std::cout << "Y axis\n";
-	std::cin >> chosenY;
-	GameBoard.updateBoard(chosenX, chosenY, whichPlayerGlobal);
-
-}
-
 int main() {
 	GameBoard.printToConsole();
-	chooseWhichOption();
+	Player1.chooseWhichOption();
 	std::cout << "Tic tac toe";
 }
