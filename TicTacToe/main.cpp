c1e12d74c975aaa2d3502b796220e963fa33812f
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-04-13 08:23:58 +0800

    Add files via upload

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -34,25 +34,26 @@ void clearGrid();
 
 
 Result move(int x, int y, Symbol symbol) {
-
+int squaresNo = 0 ;
 
 if (x < 3 && x >= 0 && y < 3 && y >= 0 && grid[y][x] == Symbol::EMPTY && grid[y][x] == EMPTY) {
 grid[y][x] = symbol;
 
-
+/* 
 for (int i = 0; i < 3; i++)
 if ((grid[i][0] == grid[i][1] && grid[i][0] == grid[i][2]) ||
-(grid[0][i] == grid[1][i] && grid[i][0] == grid[2][i]) ||
+(grid[0][i] == grid[1][i] && grid[0][i] == grid[2][i]) ||
 (grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2]) ||
 (grid[2][0] == grid[1][1] && grid[2][0] == grid[0][2]))return Result::WON;
-
+*/
 
 for (int i = 0; i < 3; i++)
 for (int j = 0; j < 3; j++)
-if (grid[i][j] != Symbol::EMPTY) return Result::LEGIT;
+if (grid[i][j] == Symbol::EMPTY) return Result::LEGIT;
+else (grid[i][j] != Symbol::EMPTY); squaresNo++;
+std::cout << "balls" <<squaresNo;
 
-
-return Result::TIE;
+if (squaresNo == 9) return Result::LEGIT;
 }
 else return Result::WRONG;
 }
@@ -98,16 +99,16 @@ std::cin >> yAxis;
 switch (whichPlayer) {
 Result a;
 case 1:
+whichPlayer = 2;
 a = move(xAxis, yAxis, Symbol::CROSS);
 checkWinner(a);
-whichPlayer = 2;
 break;
 case 2:
+whichPlayer = 1;
 a = move(xAxis, yAxis, Symbol::CIRCLE);
 checkWinner(a);
-whichPlayer = 1;
 break;
-}
+ }
 }
 
 
